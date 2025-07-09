e6d89e11b4b000471a04466535720faf1e039073
GameShopCorpGlobal <gameshopengine@gmail.com>
2025-07-09 10:21:23 -0400

    Making A Loxel

--- GameShopEngine2JME/src/main/java/com/mygame/Main.java
+++ GameShopEngine2JME/src/main/java/com/mygame/Main.java
@@ -3,9 +3,13 @@ package com.mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector4f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Quad;
+import com.jme3.texture.Texture2D;
+import com.mygame.graphics.ATMS;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -21,11 +25,15 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
+        flyCam.setEnabled(false);
+        ATMS atms = new ATMS("Loxel", 1920, 1080);
+        atms.layer.drawCircle(960, 540, 50, new Vector4f(0,0,255,255));
+        Quad q = new Quad(19.2f, 10.8f);
+        Geometry geom = new Geometry("Quad", q);
+        geom.move(-9.6f, -5.4f, 0);
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
+        mat.setColor("Color", ColorRGBA.White);
+        mat.setTexture("ColorMap", new Texture2D(atms.makeATMS()));
         geom.setMaterial(mat);
 
         rootNode.attachChild(geom);
